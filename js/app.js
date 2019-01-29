4c0d01895a9c293566c736977aed50ee02b079f0
LugurbalDev <lugurbaldev@gmail.com>
2019-01-29 21:32:20 +0100

    Ajout de la barre et de la balle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Récupère l'élément canvas
 let zoneJeu = document.getElementById('playZone')
-
+let barreX = 130
+let nbrVie = 3
 //Définit le canvas en 2D
 let canvasJeu = zoneJeu.getContext('2d')
 
@@ -24,21 +25,32 @@ function drawBrique(nombreBriques,y,couleur = "green"){
 		canvasJeu.fillRect(x,y,width,height)
 		canvasJeu.closePath()
 		x = x + 5 + width
-		//canvasJeu.moveTo(x,y)
 	}
 }
+
 
 for(let i = 0; i < 5; i++){
 	if(i == 0){
 		drawBrique(12,y,"red")
 	} else if (i == 1){
 		drawBrique(12,y,"blue")
-	} else if (i == 3){
+	} else if (i == 2){
 		drawBrique(12,y,"yellow")
-	} else if (i == 4){
+	} else if (i == 3){
 		drawBrique(12,y,"pink")
 	} else {
 		drawBrique(12,y)
 	}
 	y = y + 10
 }
+
+canvasJeu.beginPath()
+	canvasJeu.fillStyle = "gray"
+	canvasJeu.fillRect(barreX ,135 , 50, 5)
+canvasJeu.closePath()
+
+let smash_ball = new Image(100,100)
+smash_ball.src = "imgs/smash_ball.png"
+smash_ball.addEventListener('load',function(){
+    canvasJeu.drawImage(smash_ball,150,115,20,20)
+})
