88db865b3483d25992713d3307af881b29a4fc11
LugurbalDev <lugurbaldev@gmail.com>
2019-02-02 21:55:54 +0100

    2eme refonte du système de briques

--- js/app.js
+++ js/app.js
@@ -1,38 +1,17 @@
-import {drawBrique,draw} from "./modules/draw.js";
+import {draw} from "./modules/draw.js";
 
 // Récupère l'élément canvas
-let zoneJeu = document.getElementById('playZone')
+export let zoneJeu = document.getElementById('playZone')
 
 let nbrVie = 3
 //Définit le canvas en 2D
 export let canvasJeu = zoneJeu.getContext('2d')
 
 /**
- * Permet d'actualiser la position de la balle et de la barre sans effacer les briques
+ * Réactualise le canvas toutes les 10 ms
  */
-setInterval(function(){
-    canvasJeu.clearRect(0,zoneJeu.height - 53,zoneJeu.width,zoneJeu.height)
-    draw()
-},10)
 
-console.log(zoneJeu.height)
+setInterval(draw,10)
 
-// Hauteur initiale des briques dans le canvas
-export let y = 3
-
-for(let i = 0; i < 5; i++){
-	if(i == 0){
-		drawBrique("red")
-	} else if (i == 1){
-		drawBrique("blue")
-	} else if (i == 2){
-		drawBrique("yellow")
-	} else if (i == 3){
-		drawBrique("pink")
-	} else {
-		drawBrique("purple")
-	}
-	y = y + 10
-}
-
-draw()
+console.log(`Longueur de la zone de jeu : ${zoneJeu.width}px`)
+console.log(`Hauteur de la zone de jeu : ${zoneJeu.height}px`)
--- js/modules/draw.js
+++ js/modules/draw.js
@@ -1,37 +1,59 @@
-import {canvasJeu, y} from "../app.js";
-
-export let nbBriquesParLigne = 12
-
-export let nbreLignes = 5
-
-// Tableau contenant toutes les briques
-let bricks = []
+import {canvasJeu, zoneJeu} from "../app.js";
 
 //Position initiale de la barre
 let barreX = 130
+let bricks = []
+let nbBricks = 12
+let nbLines = 5
 
-/**Permet de dessiner les briques
- * @param couleur Couleur des briques
- */
-export function drawBrique(couleur = "green") {
-    let x = 2
-    let width = 20
-    let height = 5
 
-    for (let l = 0; l < nbreLignes; l++) {
-        bricks[l] = [];
-        for (let r = 0; r < nbBriquesParLigne; r++) {
-            bricks[l][r] = {x: 0, y: 0}
-            bricks[l][r].x = x
-            bricks[l][r].y = y
-            canvasJeu.beginPath()
-            canvasJeu.fillStyle = couleur
-            canvasJeu.fillRect(x, y, width, height)
-            canvasJeu.closePath()
+console.log(bricks)
+
+export function drawBricks(){
+
+    let x = 60
+    let y = 20
+    let width = 35
+    let height = 20
+    let couleur;
+    for(let c = 0; c < nbLines; c++){
+        bricks[c] = [];
+        if(c != 0){
+            x = 60
+            y = y + 30
+        }
+        switch(c){
+            case 0:
+                couleur = "red"
+                break
+            case 1:
+                couleur = "green"
+                break
+            case 2:
+                couleur = "yellow"
+                break
+            case 3:
+                couleur = "aquamarine"
+                break
+            case 4:
+                couleur = "pink"
+                break
+        }
+        for(let r = 0; r < nbBricks; r++){
+            bricks[c][r] = { x: 0, y: 0, visible : true};
+            bricks[c][r].x = x
+            bricks[c][r].y = y
+            if(bricks[c][r].visible){
+                canvasJeu.beginPath()
+                canvasJeu.fillStyle = couleur
+                canvasJeu.fillRect(x,y,width,height)
+                canvasJeu.closePath()
+            }
             x = x + width + 5
         }
     }
 }
+
 
 /**
  * Permet de dessiner la balle
@@ -40,7 +62,7 @@ function drawBall() {
     let smash_ball = new Image(100, 100)
     smash_ball.src = "imgs/smash_ball.png"
     smash_ball.addEventListener('load', function () {
-        canvasJeu.drawImage(smash_ball, 150, 115, 20, 20)
+        canvasJeu.drawImage(smash_ball, 150, 315, 35, 35)
     })
 }
 
@@ -50,7 +72,7 @@ function drawBall() {
 function drawPaddle(){
     canvasJeu.beginPath()
     canvasJeu.fillStyle = "gray"
-    canvasJeu.fillRect(barreX ,135 , 50, 5)
+    canvasJeu.fillRect(barreX ,350 , 70, 5)
     canvasJeu.closePath()
 }
 
@@ -58,6 +80,8 @@ function drawPaddle(){
  * Dessine la balle et la barre de déplacement
  */
 export function draw(){
+    canvasJeu.clearRect(0,0,zoneJeu.width,zoneJeu.height)
+    drawBricks()
     drawBall()
     drawPaddle()
 }
