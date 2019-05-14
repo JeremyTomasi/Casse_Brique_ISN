97888b7feba652198698e2a00fbaee257e0eb78f
LugurbalDev <lugurbaldev@gmail.com>
2019-05-14 09:06:26 +0200

    Ajout son

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ for(let i = 0; i < liste.length; i++){
     changeMenu(i)
 }
 
+
+let son = new Audio("../sounds/Musique_ISN.mp3")
+son.loop = true
+son.play()
 /**
  * Initialise le jeu et réactualise le canvas toutes les 10 ms
  */
